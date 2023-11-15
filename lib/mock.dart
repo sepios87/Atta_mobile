44b4b469983de7b1b27ea14cff16b38a5a8ef7e7
florianToribio <94830380+florianToribio@users.noreply.github.com>
2023-11-15 16:30:03 +0100

    mock

--- lib/mock.dart
+++ lib/mock.dart
@@ -1,56 +1,89 @@
+import 'dart:math';
+
 import 'package:atta/entities/day.dart';
 import 'package:atta/entities/dish.dart';
 import 'package:atta/entities/filter.dart';
 import 'package:atta/entities/menu.dart';
 import 'package:atta/entities/opening_time.dart';
 import 'package:atta/entities/restaurant.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 
-final mockedData = List.filled(
-  10,
-  AttaRestaurant(
-    id: 'id',
-    name: 'Papa burger',
-    imageUrl: 'https://picsum.photos/200/300',
-    category: [AttaCategoryFilter.burger],
-    address: '6 Rue Charles Michels, Limoges',
-    description:
-        'Le papaburger est un restaurant de burger végétarien. Nous proposons des burgers de qualité avec des produits frais et locaux.',
-    email: 'resto@gmail.com',
-    phone: '0123456789',
-    website: 'https://google.com',
-    openingTimes: {
-      AttaDay.tuesday: const [
-        AttaOpeningTime(
-          open: TimeOfDay(hour: 12, minute: 0),
-          close: TimeOfDay(hour: 14, minute: 0),
-        ),
-        AttaOpeningTime(
-          open: TimeOfDay(hour: 18, minute: 0),
-          close: TimeOfDay(hour: 22, minute: 0),
-        ),
-      ],
-    },
-    dishes: List.filled(
-      10,
-      AttaDish(
-        id: '1',
-        name: 'Burger',
-        imageUrl: 'https://picsum.photos/200/300',
-        description: 'Un bon burger végétarien a manger sur place ou a emporter avec de la sauce kebab',
-        price: 12,
-        ingredients: ['pain', 'viande', 'salade', 'tomate'],
+final mockedData = generateMockedData();
+
+final Random _random = Random();
+
+List<AttaRestaurant> generateMockedData() {
+  final List<AttaRestaurant> mockedData = [];
+
+  for (int i = 0; i < 50; i++) {
+    final randomImageSize = '${_random.nextInt(800) + 800}x${_random.nextInt(800) + 800}';
+
+    final restaurant = AttaRestaurant(
+      id: '$i',
+      name: faker.food.restaurant(),
+      imageUrl: 'https://source.unsplash.com/random/$randomImageSize?restaurant',
+      category: [AttaCategoryFilter.values[_random.nextInt(AttaCategoryFilter.values.length)]],
+      address: faker.address.streetAddress(),
+      description: faker.lorem.sentence(),
+      email: faker.internet.email(),
+      phone: faker.phoneNumber.random.toString(),
+      website: faker.internet.domainWord(),
+      openingTimes: _generateOpeningTimes(),
+      dishes: _generateDishes(),
+      menus: _generateMenus(),
+    );
+    mockedData.add(restaurant);
+  }
+
+  return mockedData;
+}
+
+List<AttaDish> _generateDishes() {
+  return List.generate(_random.nextInt(10) + 1, (index) {
+    final randomImageSize = '${_random.nextInt(800) + 800}x${_random.nextInt(800) + 800}';
+
+    return AttaDish(
+      id: '$index',
+      name: faker.food.dish(),
+      imageUrl: 'https://source.unsplash.com/random/$randomImageSize?dish',
+      description: faker.lorem.sentence(),
+      price: _random.nextDouble() * 20,
+      ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
+    );
+  });
+}
+
+List<AttaMenu> _generateMenus() {
+  return List.generate(_random.nextInt(5) + 1, (index) {
+    final randomImageSize = '${_random.nextInt(800) + 800}x${_random.nextInt(800) + 800}';
+
+    return AttaMenu(
+      id: '$index',
+      name: faker.lorem.word(),
+      imageUrl: 'https://source.unsplash.com/random/$randomImageSize?menu',
+      description: faker.lorem.sentence(),
+      price: _random.nextDouble() * 30,
+      dishIds: List.generate(
+        _random.nextInt(3) + 1,
+        (index) => '$index',
       ),
-    ),
-    menus: [
-      AttaMenu(
-        id: '1',
-        name: 'Menu',
-        imageUrl: 'https://picsum.photos/200/300',
-        description: 'lorem ipsum dolor sit amet',
-        price: 12,
-        dishIds: ['1'],
+    );
+  });
+}
+
+Map<AttaDay, List<AttaOpeningTime>> _generateOpeningTimes() {
+  final Map<AttaDay, List<AttaOpeningTime>> openingTimes = {};
+
+  for (final day in AttaDay.values) {
+    openingTimes[day] = List.generate(
+      _random.nextInt(3) + 1,
+      (index) => AttaOpeningTime(
+        open: TimeOfDay(hour: _random.nextInt(12), minute: _random.nextInt(60)),
+        close: TimeOfDay(hour: _random.nextInt(12) + 12, minute: _random.nextInt(60)),
       ),
-    ],
-  ),
-);
+    );
+  }
+
+  return openingTimes;
+}
