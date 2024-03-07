c2ea6bab6297885f6f148066b86d62f1f13879d1
florian.toribio <florian.toribio@odandb.com>
2024-03-07 09:46:58 +0100

    update timeofday + translations

--- lib/extensions/time_of_day_ext.dart
+++ lib/extensions/time_of_day_ext.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 
 extension TimeOfDayExt on TimeOfDay {
   /// Converts [TimeOfDay] to [DateTime] with today's date.
-  DateTime toDateTime() {
-    final now = DateTime.now();
-    return DateTime(now.year, now.month, now.day, hour, minute);
+  String toHHmm() {
+    return '${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}';
+  }
+
+  static TimeOfDay fromHHmm(String hhmm) {
+    final parts = hhmm.split(':');
+    return TimeOfDay(hour: int.parse(parts[0]), minute: int.parse(parts[1]));
   }
 
   int get inMinutes => hour * 60 + minute;
--- lib/pages/auth/auth_page.dart
+++ lib/pages/auth/auth_page.dart
@@ -10,10 +10,11 @@ import 'package:atta/theme/spacing.dart';
 import 'package:atta/theme/text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_translate/flutter_translate.dart';
 
 part 'widgets/password_field.dart';
 part 'widgets/login.dart';
-part 'widgets/register.dart';
+part 'widgets/create_account.dart';
 
 class AuthPage {
   static const path = '/auth';
@@ -45,34 +46,43 @@ class _AuthScreen extends StatelessWidget {
         ),
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(AttaSpacing.xl),
-          child: BlocConsumer<AuthCubit, AttaAuthState>(
-            listener: (context, state) async {
-              final status = state.status;
+          child: Column(
+            children: [
+              const SizedBox(height: AttaSpacing.xl),
+              Text(translate('auth_page.welcome'), style: AttaTextStyle.header.copyWith(fontSize: 36)),
+              const SizedBox(height: AttaSpacing.xl),
+              Text(translate('auth_page.content'), textAlign: TextAlign.center, style: AttaTextStyle.content),
+              const SizedBox(height: AttaSpacing.l),
+              BlocConsumer<AuthCubit, AttaAuthState>(
+                listener: (context, state) async {
+                  final status = state.status;
 
-              if (status is AuthErrorStatus) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(status.message),
-                    backgroundColor: Colors.red,
-                  ),
-                );
-              }
+                  if (status is AuthErrorStatus) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(status.message),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                  }
 
-              if (status is AuthSuccessStatus) {
-                final user = userService.user;
-                if (!state.isLogin && user != null) {
-                  await showEditProfileBottomSheet(context, user);
-                }
-                // ignore: use_build_context_synchronously
-                context.adaptativePopNamed(HomePage.routeName);
-              }
-            },
-            buildWhen: (previous, current) => previous.isLogin != current.isLogin,
-            builder: (context, state) {
-              if (state.isLogin) return const _LoginContent();
+                  if (status is AuthSuccessStatus) {
+                    final user = userService.user;
+                    if (!state.isLogin && user != null) {
+                      await showEditProfileBottomSheet(context, user);
+                    }
+                    // ignore: use_build_context_synchronously
+                    context.adaptativePopNamed(HomePage.routeName);
+                  }
+                },
+                buildWhen: (previous, current) => previous.isLogin != current.isLogin,
+                builder: (context, state) {
+                  if (state.isLogin) return const _LoginContent();
 
-              return const _RegisterContent();
-            },
+                  return const _CreateAccountContent();
+                },
+              ),
+            ],
           ),
         ),
       ),
