123b4843e11a40e0ce3393995636110c98c5620a
florian.toribio <florian.toribio@odandb.com>
2024-05-30 15:14:13 +0200

    test

---
+++ ok.bash
@@ -0,0 +1,20 @@
+output_code=$(flutter pub run dart_code_linter:metrics check-unused-code lib --no-congratulate)
+          nb_unused_code=$(echo $output_code | rev | cut -f1 -d' ')
+          if [ -z $nb_unused_code ]; then 
+            message="✅ Aucun code non utilisé"
+            echo $message
+            echo $message >> $GITHUB_STEP_SUMMARY
+          else
+            if (( $nb_unused_code > 0 )); then
+              message="⚠️ Quantité de code non utilisée : $nb_unused_code"
+              echo $message
+              echo "$output_code"
+            fi
+            if (( $nb_unused_code > 5 )); then
+              message="❌ Le projet a trop de code inutilisé ($nb_unused_code), il faut ${{ inputs.max_unused_code }} maximum"
+              echo $message
+              echo $output_code
+              exit 1
+            fi
+          fi
+echo "coucou"
