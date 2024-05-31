fe08ac36ad236f013cb0ac9694dff35ef9654751
florian.toribio <florian.toribio@odandb.com>
2024-05-31 14:49:37 +0200

    test

--- ok.bash
+++ ok.bash
@@ -1,5 +1,3 @@
 outdated_dep=$(flutter pub outdated --json --no-prereleases --no-dev-dependencies)
 back=$(echo "$outdated_dep" | grep -B 5 '"kind": "direct"' | grep '"package":' | cut -d '"' -f 4)
-echo "$back"
-nb=$(echo "$back" | wc -l)
-echo "$nb"
+echo "$back" | paste -sd "," -
