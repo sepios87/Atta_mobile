43e9728ec1698b95fb89dfcb5a963b4b6c014f7d
florian.toribio <florian.toribio@odandb.com>
2024-05-31 14:46:26 +0200

    update

--- ok.bash
+++ ok.bash
@@ -1,15 +1,5 @@
- outdated_dep=$(flutter pub outdated --json --no-prereleases --no-dev-dependencies)
-          nb_outdated_dep=$(echo $outdated_dep | jq '[.packages[] | select(.kind == "direct")] | length' <<< "$outdated_dep")
-          message="Nombre de paquets a mettre a jour en version majeure : $nb_outdated_dep"
-          echo $message
-          echo $message >> $GITHUB_STEP_SUMMARY
-          if (( $nb_outdated_dep > ${{ inputs.max_obsolete_packages }} ));
-           then
-            message="❌ Le projet a trop de dépendances obsolètes ($nb_outdated_dep), il faut ${{ inputs.max_obsolete_packages }} maximum"
-            echo $message
-            echo $message >> $GITHUB_STEP_SUMMARY
-            upgradable_dep=$(echo $outdated_dep  | jq '[.packages[] | select(.kind == "direct") | {package: .package, upgradable_version: .upgradable.version}]')
-            echo $upgradable_dep
-            echo $upgradable_dep >> $GITHUB_STEP_SUMMARY
-            exit 1
-          fi
+outdated_dep=$(flutter pub outdated --json --no-prereleases --no-dev-dependencies)
+back=$(echo "$outdated_dep" | grep -B 5 '"kind": "direct"' | grep '"package":' | cut -d '"' -f 4)
+echo "$back"
+nb=$(echo "$back" | wc -l)
+echo "$nb"
